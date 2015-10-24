b4ca59268a7123e0fb8f965a3170b5019eb9f65c
FloridaGeologicalSurvey <Seth.Bassett@dep.state.fl.us>
2015-10-24 13:22:05 -0400

    exercise complete
    Exercise is complete, code tested and run.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,70 @@
-## Put comments here that give an overall description of what your
-## functions do
+##########
+# Author: Seth Bassett
+# Date: 24 October 2015
+# Organization: Florida Geological Survey
+# Context: Coursera Data Science,
+#            Module #2 (R Programming), 
+#               Programming Assignment #2
+#
+# Special matrices objects for caching matrix transpositions
+# Useful for optimizing multiple or sequential calls to t() by caching output
+#
+##########
 
-## Write a short comment describing this function
+# Creates a makeCacheMatrix object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # param x (matrix) a matrix object
+  
+  #create transposition variable
+  trans <- NULL
+  
+  #set function - sets matrix and transposition variables
+  set <- function(y) {
+    x <<- y
+    trans <<- NULL
+  }
+  
+  #return original matrix (x)
+  get <- function() x
+  
+  #calculate transposition of original matrix and cache (x)
+  settrans <- function(t) trans <<- t
+  
+  #return cached transposition variable
+  gettrans <- function() trans
+  
+  #returns list of functions, accessible via '$' operator
+  list(set = set, 
+       get = get, 
+       settrans = settrans, 
+       gettrans = gettrans)
 }
 
 
-## Write a short comment describing this function
 
+# Sets a cache matrid for a makeCacheMatrix object 
+# OR returns a previously cached transposition
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # param x (makeCacheMatrix) a makeCacheMatrix object
+  
+  # call the transposition from makeCacheMatrix object
+  trans <- x$gettrans()
+  
+  # if cached transposition is not null
+  # send message to console that the return is a cached value
+  # and return cache
+  if(!is.null(trans)) {
+    #message console
+    message("getting cached transposition")
+    
+    #break and exit function here by returning cached transposition
+    return(trans)
+  }
+  
+  #otherwise, calculate transposition and return
+  data <- x$get()
+  trans <- t(data, ...)
+  x$settrans(trans)
+  trans
 }
